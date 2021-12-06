76b620dd2892b2bc5b1ad4d2237b5b757ffb81e9
Hui Li <lihuifree@gmail.com>
2021-12-06 11:09:02 +0800

    tcp 多server

--- app.js
+++ app.js
@@ -1,59 +1,79 @@
-'use strict';
+'use strict'
 
-const net = require('net');
-const path = require('path');
-const PLUGIN_NAME = 'tcp';
+const net = require('net')
+const path = require('path')
+const PLUGIN_NAME = 'tcp'
 
-function resolveController(controller, app) {
-    if (typeof controller === 'string') {
-        const actions = controller.split('.');
-        let obj = app[PLUGIN_NAME].controller;
-        actions.forEach(key => {
-            obj = obj[key];
-            if (!obj) throw new Error(`controller '${controller}' not exists`);
-        });
-        controller = obj;
-    }
-    // ensure controller is exists
-    if (!controller) throw new Error('controller not exists');
-    return controller;
+function resolveController(controller, app, index) {
+  if (typeof controller === 'string') {
+    const actions = controller.split('.')
+    let obj = app[PLUGIN_NAME].controller[index]
+    actions.forEach(key => {
+      obj = obj[key]
+      if (!obj) throw new Error(`controller '${controller}' not exists`)
+    })
+    controller = obj
+  }
+  // ensure controller is exists
+  if (!controller) throw new Error('controller not exists')
+  return controller
 }
 
 function EggTCP(app) {
-    this.app = app;
-    const server = net.createServer();
-    server.listen(app.config.tcp.port, app.config.tcp.host);
-    this.server = server
+  this.app = app
+  this.server = []
+  for (let index = 0; index < app.config.tcp.length; index++) {
+    const tcp = app.config.tcp[index]
+    const server = net.createServer()
+    server.listen(tcp.port, tcp.host)
+    this.server[index] = server
+  }
 }
 
-EggTCP.prototype.handle = function (handler) {
-    const controller = resolveController(handler, this.app);
-    
-    this.server.on('error', (err) => {
-        this.app.coreLogger.error(`egg-tcp error:\n${err.stack}`);
-    });
-    
-    // When a client requests a connection with the server, the server creates a new
-    // socket dedicated to that client. The socket was handled by controller.
-    this.server.on('connection', controller);
-    
-    this.server.on('listening', () => {
-        const address = this.server.address();
-        this.app.coreLogger.info(`egg-tcp listening ${address.address}:${address.port}`);
-    });
-};
+EggTCP.prototype.handle = function (handler, index) {
+  const controller = resolveController(handler, this.app, index)
+
+  this.server[index].on('error', err => {
+    this.app.coreLogger.error(`egg-tcp error:\n${err.stack}`)
+  })
+
+  // When a client requests a connection with the server, the server creates a new
+  // socket dedicated to that client. The socket was handled by controller.
+  this.server[index].on('connection', controller)
+
+  this.server[index].on('listening', () => {
+    const address = this.server[index].address()
+    this.app.coreLogger.info(
+      `egg-tcp listening ${address.address}:${address.port}`,
+    )
+  })
+}
 
 module.exports = app => {
-    app[PLUGIN_NAME] = new EggTCP(app);
-    app[PLUGIN_NAME].controller = app[PLUGIN_NAME].controller || {};
-    
-    app.beforeClose(function () {
-        app[PLUGIN_NAME].server.close()
-    });
-    
+  app[PLUGIN_NAME] = new EggTCP(app)
+  if (!app[PLUGIN_NAME].controller) {
+    app[PLUGIN_NAME].controller = []
+    for (const server of app[PLUGIN_NAME].server) {
+      app[PLUGIN_NAME].controller.push({})
+    }
+  }
+  app[PLUGIN_NAME].controller = app[PLUGIN_NAME].controller || []
+
+  app.beforeClose(function () {
+    for (const server of app[PLUGIN_NAME].server) {
+      server.close()
+    }
+  })
+  for (let index = 0; index < app[PLUGIN_NAME].server.length; index++) {
     new app.loader.FileLoader({
-        directory: path.join(app.config.baseDir, 'app', PLUGIN_NAME, 'controller'),
-        target: app[PLUGIN_NAME].controller,
-        inject: app,
-    }).load();
-};
+      directory: path.join(
+        app.config.baseDir,
+        'app',
+        PLUGIN_NAME,
+        'controller',
+      ),
+      target: app[PLUGIN_NAME].controller[index],
+      inject: app,
+    }).load()
+  }
+}
